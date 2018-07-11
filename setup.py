c52df5335ab4efa14e38bd946b228db36dd1caf8
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-07-11 06:48:21 -0400

    Added more strings classifiers items for the metadata package

--- setup.py
+++ setup.py
@@ -5,19 +5,37 @@ version = '1.0'
 
 setup(name='diazotheme.framework.metro',
       version=version,
-      description="",
+      description="A Diazo framework implementation for the Metro UI CSS framework",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
+      # https://pypi.org/pypi?:action=list_classifiers
       classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: Theme",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      keywords='web zope plone theme diazo metro-ui css framework',
+      author='Thijs Jonkman',
+      author_email='t.jonkman@gmail.com',
+      maintainer='Leonardo Caballero',
+      maintainer_email='leonardocaballero@gmail.com',
+      url='https://github.com/TH-code/diazotheme.framework.metro',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['diazotheme', 'diazotheme.framework'],
@@ -25,6 +43,7 @@ setup(name='diazotheme.framework.metro',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'diazoframework.plone',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
